76a433fc8fc2ca8073e9e21b8bba5d094907329f
Dilshan Tharuka <dilshanchamith99@gmail.com>
2024-03-05 00:06:45 +0530

    first commit

---
+++ P/cartn.js
@@ -0,0 +1,110 @@
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+
+cartIcon.onclick = () =>{
+    cart.classList.add("active");
+}
+
+closeCart.onclick = () =>{
+    cart.classList.remove("active");
+}
+
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready);
+}else{
+    ready();
+}
+
+function ready(){
+    var removeCartButtons = document.getElementsByClassName('cart-remove');
+    for (var i=0; i<removeCartButtons.length; i++){
+        var button = removeCartButtons[i];
+        button.addEventListener('click', removeCartItem);
+    }
+    var quantityInputs = document.getElementsByClassName('cart-quantity');
+    for (var i=0; i<quantityInputs.length; i++){
+        var input = quantityInputs[i];
+        input.addEventListener('change', quantityChanged);
+    }
+    //add to cart 
+    var addCart = document.getElementsByClassName('add-cart');
+    for (var i=0; i< addCart.length; i++){
+        var button = addCart[i];
+        button.addEventListener('click', addCartClicked);
+    }
+
+}
+
+
+
+
+function removeCartItem(event){
+    var buttonClicked =event.target;
+    buttonClicked.parentElement.remove(); 
+    updatetotal();
+}
+
+function quantityChanged(event){
+    var input = event.target;
+    if(isNaN(input.value) || input.value <= 0){
+        input.value=1;
+    }
+    updatetotal();
+}
+//add cart function
+function addCartClicked(event){
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName('p-name')[0].innerText;
+    var price =shopProducts.getElementsByClassName('p-price')[0].innerText;
+    var productImg = shopProducts.getElementsByClassName('img')[0].src
+    addProductToCart(title,price,productImg);
+    updatetotal();
+}
+
+function addProductToCart(title,price,productImg){
+    var cartShopBox = document.createElement('div');
+    cartShopBox.classList.add('cart-box');
+    var cartItems = document.getElementsByClassName('cart-content')[0];
+    var cartItemsNames = cartItems.getElementsByClassName('cart-product-title');
+    for( var i=0; i< cartItemsNames.length; i++){
+        if(cartItemsNames[i].innerText == title) {
+            alert('You have already added this item to cat');
+            return;
+        }
+    }
+    var cartBoxContent = `
+    < img src="${productImg}" alt="" class="img cart-img"/>
+    <div class="details-box">
+        <div class="cart-product-title"> ${title} </div>
+        <div class="cart-price"> ${price}</div>
+        <input type="number" name="" id="" value="1" class ="cart-quantity">
+
+    </div>
+        <i class ="bx bx-trash-alt cart-remove"></i>`
+        cartShopBox.innerHTML = cartBoxContent;
+        cartIcon.append(cartShopBox);
+        cartShopBox.getElementsByClassName('cart-remove')[0]
+        .addEventListener('click', removeCartItem);
+        cartShopBox.getElementsByClassName('cart-quantity')[0]
+        .addEventListener('change',quantityChanged);
+}
+
+
+function updatetotal(){
+    var cartContent = document.getElementsByClassName('cart-content')[0];
+    var cartBoxes = cartContent.getElementsByClassName('cart-box');
+    var total=0;
+    for(var i=0; i < cartBoxes.length; i++){
+        var cartBox =cartBoxes[i];
+        var priceElement = cartBox.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartBox.getElementsByClassName('cart-quantity')[0];
+        var price=parseFloat(priceElement.innerText.replace('$', ''))
+        var quantity=quantityElement.value;
+        total+= price * quantity ;
+
+        document.getElementsByClassName('total-price')[0].innerText ='$' + total;
+
+    }
+}
---
+++ P/search1.js
@@ -0,0 +1,21 @@
+const search = () =>{
+    const searchbox  =document.getElementById("search-item").value.toUppercase();
+    const storeitems =document.getElementById("product-list")
+    const product= document.querySelectorAll(".product")
+    const pname=storeitems.getElementsByTagName("h2")
+
+    for(var i = 0 ; i < pname.length; i++){
+        let match = product[i].getElementsByTagName('h2')[0];
+
+        if(match){
+          let textvalue =  match.textContent || match.innerHTML
+
+          if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+            product[i].search1.display="";
+        }else{
+            
+            product[i].search1.display="none";
+        } 
+        }
+    }
+}
